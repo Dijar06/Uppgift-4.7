15b73498f35156c53e481e2ecc3f9ed1953c7bdd
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-23 15:06:37 +0200

    Add project files.

---
+++ Uppgift 4.7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett meddelande som du vill se vertikalt:");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hur många steg åt höger ska ditt meddelande flyttas?");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i  = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
